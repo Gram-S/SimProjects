25b0220e1778dc0b841d4635a8d745850eddcb5f
Gram-S <grantms4455@gmail.com>
2026-01-22 10:46:06 -0700

    add ball

--- boxball.py
+++ boxball.py
@@ -1,3 +1,11 @@
 
 
-class ball()
+class Ball:
+
+    def __init__(self, position_x, position_y, velocity_at_x, velocity_at_y):
+        self.position_x = position_x
+        self.position_y = position_y
+        self.velocity_at_x = velocity_at_x
+        self.velocity_at_y = velocity_at_y
+
+    def update_position_x
