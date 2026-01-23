d7a6108a79c7d31e4d75ff5aba434b54e092f7de
Gram-S <grantms4455@gmail.com>
2026-01-23 15:45:57 -0700

    ball class

--- boxball.py
+++ boxball.py
@@ -8,4 +8,29 @@ class Ball:
         self.velocity_at_x = velocity_at_x
         self.velocity_at_y = velocity_at_y
 
-    def update_position_x
+    def update_position_x(self, new_x):
+        self.position_x = new_x
+
+    def update_position_y(self, new_y):
+        self.position_y = new_y
+
+    def update_velocity_x(self, new_x):
+        self.velocity_at_x = new_x
+
+    def update_velocity_y(self, new_y):
+        self.velocity_at_y = new_y
+    
+    # Changes what is output with print
+    def __str__(self):
+        return f"ball lol \nPosition at X: {self.position_x} \nPosition at Y: {self.position_y} \nVelocity at X: {self.velocity_at_x} \nVelocity at Y: {self.velocity_at_y}"
+        
+
+
+if __name__ == "__main__":
+    ball = Ball(0, 0, 0, 0)
+    print(ball) # Expected all 0s
+    ball.update_position_x(1)
+    ball.update_position_y(1)
+    ball.update_velocity_x(1)
+    ball.update_velocity_y(1)
+    print(ball) # Expected all 1s
