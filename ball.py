16619378c8896a8a2874582f611a99a032b5c0a2
Gram-S <grantms4455@gmail.com>
2026-01-23 17:15:20 -0700

    accessability functions for ball

--- ball.py
+++ ball.py
@@ -4,12 +4,29 @@ import matplotlib.pylab as plt
 class Ball:
 
     def __init__(self, position_x, position_y, velocity_at_x, velocity_at_y):
-        self.name = self
         self.state_vector = np.array([position_x, position_y, velocity_at_x, velocity_at_y])
 
+    def position_x(self):
+        return self.state_vector[0]
+
+    def position_y(self):
+        return self.state_vector[1]
+
+    def velocity_x(self):
+        return self.state_vector[2]
+
+    def velocity_y(self):
+        return self.state_vector[3]
+
+    # Probably an uneeded function
+    def get_state_vector(self):
+        return self.state_vector
+    
+
+        
     def euler_update_all(self, function, time_step):
         self.state_vector =  self.state_vector + function(self.state_vector) * time_step
-        
+            
     # Changes what is output with print
     def __str__(self):
         return f"\nball lol \n[x y vx vy]\n{self.state_vector}"
