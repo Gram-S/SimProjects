6795fa577ff6f80c64f8ff13c352a3715aae17f8
Leo-Petersen <dp191252@umconnect.umt.edu>
2026-01-25 15:28:24 -0700

    Added boxball.py and boxball_radius.py. Two different approaches

--- boxball_radius.py
+++ boxball_radius.py
@@ -1,15 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from matplotlib.patches import Circle
 
 class Ball:
 
     # Set up the ball with initial position and velocity
-    def __init__(self, position_x, position_y, velocity_at_x, velocity_at_y):
+    def __init__(self, position_x, position_y, velocity_at_x, velocity_at_y, radius):
         self.position_x = position_x
         self.position_y = position_y
         self.velocity_at_x = velocity_at_x
         self.velocity_at_y = velocity_at_y
+        self.radius = radius
 
     # Returns the state as an array [x,y,vx,vy]
     def state(self):
@@ -37,7 +39,7 @@ def euler(x, f, dt):
     return x + f(x) * dt
 
 
-def box_ball(x):
+def box_ball(x, radius):
     """
     INPUTS:
         x is an array, in this problem assume it contains [x,y,vx,vy],
@@ -52,15 +54,16 @@ def box_ball(x):
 
     # Check wall collisions and flip velocities
     # Right wall (x > 1) or Left wall (x < -1)
-    if pos_x > 1 or pos_x < -1:
+    if pos_x > 1 - radius or pos_x < -1 + radius:
         x[2] = -vel_x
     # Top wall (y > 1) or Bottom wall (y < -1)
-    if pos_y > 1 or pos_y < -1:
+    if pos_y > 1 - radius or pos_y < -1 + radius:
         x[3] = -vel_y
 
     # Return derivatives: [dx/dt, dy/dt, dvx/dt, dvy/dt]
     # dx/dt = vx, dy/dt = vy, no acceleration so dvx/dt = 0, dvy/dt = 0
     return np.array([x[2], x[3], 0, 0])
+
 
 if __name__ == "__main__":
     # Initial conditions
@@ -69,10 +72,10 @@ if __name__ == "__main__":
     x = 0
     y = 0
     x0 = np.array([x,y,vx,vy])
-    ball = Ball(x0[0], x0[1], x0[2], x0[3])
+    ball = Ball(x0[0], x0[1], x0[2], x0[3], radius=0.04)
 
     # Simulation parameters
-    dt = 0.01
+    dt = 0.001
     end_time = 100
 
     # Storage for plotting
@@ -88,28 +91,30 @@ if __name__ == "__main__":
         Y.append(state[1])
 
         # Update state using euler
-        new_state = euler(state, box_ball, dt)
+        new_state = euler(state, lambda x: box_ball(x, ball.radius), dt)
         ball.update_state(new_state)
 
         t += dt
 
     # Plot the results
-    plt.plot([-1,-1,1,1,-1],[-1,1,1,-1,-1],'r')
-    plt.plot(X,Y,'k')
-    plt.show()
+    # plt.plot([-1,-1,1,1,-1],[-1,1,1,-1,-1],'r')
+    # plt.plot(X,Y,'k')
+    # plt.show()
     
     ## The following is brought to you by Claude ##
     ### ANIMATION (uses same X, Y data from above) ###
     # Blitting redraws only the ball each frame instead of the whole figure
-    # fig, ax = plt.subplots()
-    # ax.plot([-1,-1,1,1,-1], [-1,1,1,-1,-1], 'r')
-    # ax.set_xlim(-1.2, 1.2)
-    # ax.set_ylim(-1.2, 1.2)
-    # ball_dot, = ax.plot([], [], 'bo', markersize=8)
+    fig, ax = plt.subplots()
+    ax.plot([-1,-1,1,1,-1], [-1,1,1,-1,-1], 'r')
+    ax.set_xlim(-1.2, 1.2)
+    ax.set_ylim(-1.2, 1.2)
+    ax.set_aspect('equal')
+    ball_circle = Circle((X[0], Y[0]), ball.radius, color='blue')
+    ax.add_patch(ball_circle)
 
-    # def animate(i):
-    #     ball_dot.set_data([X[i]], [Y[i]])
-    #     return ball_dot,
+    def animate(i):
+        ball_circle.center = (X[i], Y[i])
+        return ball_circle,
 
-    # anim = FuncAnimation(fig, animate, frames=len(X), interval=1, blit=True)
-    # plt.show()
+    anim = FuncAnimation(fig, animate, frames=len(X), interval=1, blit=True)
+    plt.show()
