918f563fbb166f37b98fdc1b341d58fdeb66a675
Gram-S <grantms4455@gmail.com>
2026-01-26 17:03:16 -0700

    more functionality for balls

--- boxball_radius.py
+++ boxball_radius.py
@@ -1,10 +1,10 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib.animation import FuncAnimation
 from matplotlib.patches import Circle
 
-class Ball:
 
+class Ball:
     # Set up the ball with initial position and velocity
     def __init__(self, position_x, position_y, velocity_at_x, velocity_at_y, radius):
         self.position_x = position_x
@@ -12,11 +12,46 @@ class Ball:
         self.velocity_at_x = velocity_at_x
         self.velocity_at_y = velocity_at_y
         self.radius = radius
+        self.x_memory = []
+        self.y_memory = []
+
+    def add_to_memory(self):
+        self.x_memory.append(self.position_x)
+        self.y_memory.append(self.position_y)
+
+    def get_x_memory(self):
+        return self.x_memory
+
+    def get_y_memory(self):
+        return self.y_memory
 
     # Returns the state as an array [x,y,vx,vy]
     def state(self):
-        return np.array([self.position_x, self.position_y, self.velocity_at_x, self.velocity_at_y])
-    
+        return np.array(
+            [self.position_x, self.position_y, self.velocity_at_x, self.velocity_at_y]
+        )
+
+    def plot(self):
+        plt.plot(self.x_memory, self.y_memory, "k")
+        plt.show()
+
+    def animate(self, plot_bounds=[-1, -1, 1, 1, -1], xlim=1.2, ylim=1.2, color="r"):
+        fig, ax = plt.subplots()
+        ax.plot(plot_bounds, list(reversed(plot_bounds)), "r")
+        ax.set_xlim(-xlim, xlim)
+        ax.set_ylim(-ylim, ylim)
+        ax.set_aspect("equal")
+        
+        ball_circle = Circle((self.x_memory[0], self.y_memory[0]), self.radius, color="blue")
+        ax.add_patch(ball_circle)
+        
+        def animate(i):
+            ball_circle.center = (self.x_memory[i], self.y_memory[i])
+            return (ball_circle,)
+        
+        anim = FuncAnimation(fig, animate, frames = len(self.x_memory), interval = 1, blit = True)
+        plt.show()
+
     # X is an array [x,y,vx,vy], this is done to make it easier to use euler
     def update_state(self, X):
         self.position_x = X[0]
@@ -35,7 +70,7 @@ def euler(x, f, dt):
         dt is the time step for the simulation.
     OUTPUTS:
         an array, corresponding to the updated version of the input x
-    """ 
+    """
     return x + f(x) * dt
 
 
@@ -71,7 +106,7 @@ if __name__ == "__main__":
     vy = -8
     x = 0
     y = 0
-    x0 = np.array([x,y,vx,vy])
+    x0 = np.array([x, y, vx, vy])
     ball = Ball(x0[0], x0[1], x0[2], x0[3], radius=0.04)
 
     # Simulation parameters
@@ -79,42 +114,26 @@ if __name__ == "__main__":
     end_time = 100
 
     # Storage for plotting
-    X = []
-    Y = []
 
     # Main simulation loop
     t = 0
     while t < end_time:
         # Get current state and store position
-        state = ball.state()
-        X.append(state[0])
-        Y.append(state[1])
+        ball.add_to_memory()
 
         # Update state using euler
-        new_state = euler(state, lambda x: box_ball(x, ball.radius), dt)
+        new_state = euler(ball.state(), lambda x: box_ball(x, ball.radius), dt)
         ball.update_state(new_state)
 
         t += dt
 
-    # Plot the results
-    # plt.plot([-1,-1,1,1,-1],[-1,1,1,-1,-1],'r')
-    # plt.plot(X,Y,'k')
-    # plt.show()
-    
-    ## The following is brought to you by Claude ##
-    ### ANIMATION (uses same X, Y data from above) ###
-    # Blitting redraws only the ball each frame instead of the whole figure
-    fig, ax = plt.subplots()
-    ax.plot([-1,-1,1,1,-1], [-1,1,1,-1,-1], 'r')
-    ax.set_xlim(-1.2, 1.2)
-    ax.set_ylim(-1.2, 1.2)
-    ax.set_aspect('equal')
-    ball_circle = Circle((X[0], Y[0]), ball.radius, color='blue')
-    ax.add_patch(ball_circle)
+        # Plot the results
+        # plt.plot([-1,-1,1,1,-1],[-1,1,1,-1,-1],'r')
+        # plt.plot(X,Y,'k')
+        # plt.show()
 
-    def animate(i):
-        ball_circle.center = (X[i], Y[i])
-        return ball_circle,
+        ## The following is brought to you by Claude ##
+        ### ANIMATION (uses same X, Y data from above) ###
+        # Blitting redraws only the ball each frame instead of the whole figure
 
-    anim = FuncAnimation(fig, animate, frames=len(X), interval=1, blit=True)
     plt.show()
